ce5cb2c0a17425bd4083f0ef88b3c944c2694d67
kirasq <ideal_sq@126.com>
2018-01-17 18:32:39 +0800

    Test

---
+++ Study001/HelloPython.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/17 18:10
+# @Author  : Kira
+# @Email   : kira-qing.sun@cn-bi.com
+# @File    : HelloPython.py
+# @Software: PyCharm
+
+# Ctrl+/ 行注释
+# print('Hello Python')
+name = 'kira'
+print('您输入的名字是'+name)
+
+# name2 = input()
+# print('hello,', name2)
+print('ABC'.encode('utf-8'))
+print('%2d-%02d' % (3, 1))
+print('%.2f' % 3.1415926)
+
+
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+
+print(len(classmates))
